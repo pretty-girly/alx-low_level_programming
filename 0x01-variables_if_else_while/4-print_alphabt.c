ec9a862aaa1986a38839653a2563c79207048399
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-06-05 14:25:09 -0700

    Commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-
-/**
- *  main - Prints some letters of alphabet
+#include <stdio.h>                                                
+                                                               
+/**                                                               
  *
- *   Return: Always (Success)
- */
-int main(void)
-{
-	char c;
+ *  *   * main - Prints some letters of alphabet                     
+ *
+ *   *     *                                                          
+ *
+ *    *       * Return: Always (Success)                               
+ *
+*/                                                                
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
- 		{
-			putchar(c);
-		 }
-	}
-
-	putchar ('\n');
-
-	return (0);
+int main(void)                                                    
+{                                                                 
+	                char c;                                           
+                                
+	                for (c = 'a'; c <= 'z'; c++)                      
+	                {                                                 
+	                        if (c != 'e' && c != 'q')                 
+	                        {                                         
+	                                putchar(c);                       
+	                        }                                         
+	                }                                                 
+                                                          
+	                putchar('\n');                                    
+		                                                                  
+	                return (0);                                       
 }
