d91a599fd840f8168d4ac913bae7d4709e83ebf9
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-06-07 20:27:02 -0700

    Task 101

---
+++ 101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 52 fibonacci numbers
+ *
+ * Return: Nothing!
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2
+		while (i < 50
+		
+		{
+int i = 0;
+long j = 1, k = 2;
+while (i < 50)
+}
+k += j;
+j = k - j;
+printf(", %ld", k);
+
+}
+
+++!
+}
+printf("\n");
+return (0);
+
+}
