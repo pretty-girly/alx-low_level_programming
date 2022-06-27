298b4b01bcf72011c7432c7395127cc4cb78b7ea
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-06-27 12:44:07 -0700

    Task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,7 @@ int count_words(char *str);
 /**
  *  word_len - Locates the index marking the end of the
  *   first word contained within a string
- *   @str: The string to be searched.
+ *   @str: The string to be searched
  *
  *   Return: The index marking the end of the initial word pointed to by str
  */
@@ -27,7 +27,7 @@ int word_len(char *str)
 
 /**
  * count_words - Counts the number of words contained within a string
- * @str: The string to be searched.
+ * @str: The string to be searched
  *
  * Return: The number of words contained within str
  */
@@ -51,7 +51,7 @@ int count_words(char *str)
 }
 
 /**
- * strtow - Splits a string into words.
+ * strtow - Splits a string into words
  * @str: The string to be split
  *
  * Return: If str = NULL, str = "", or the function fails - NULL
