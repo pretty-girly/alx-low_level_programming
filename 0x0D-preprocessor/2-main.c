b74da6eb260e0db98eb7f6730a9b72cedbeb851e
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-07-08 19:47:00 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Prints the name of the file of the program
- *  was compiled from, followed by a new line
+ *  was compiled from, followed by a new line.
  *
- *  Return: Always 0
+ *  Return: Always 0.
  */
 int main(void)
 {
