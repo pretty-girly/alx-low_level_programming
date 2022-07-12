2ad130a1db9efe17902754f5ede541d40d086dd9
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-07-12 08:09:00 -0700

    commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+
 /**
  * struct printer - A new struct type defining a printer
  * @symbol: A symbol representing a data type
@@ -12,6 +13,7 @@ typedef struct printer
 {
 	char *symbol;
 	void (*print)(va_list arg);
+
 }printer_t;
 
 int sum_them_all(const unsigned int n, ...);
