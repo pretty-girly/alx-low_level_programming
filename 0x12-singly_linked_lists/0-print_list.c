3ceaa0442f731aae125d07c977f7be765588b4ee
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-07-14 06:40:18 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t list
- * @h: The list_t list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: The number of nodes in h
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
