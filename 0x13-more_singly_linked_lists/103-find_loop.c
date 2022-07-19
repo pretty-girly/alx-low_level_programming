79576e0bfe1fb8788818021dcb2d91cf476c4a5d
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-07-19 13:34:19 -0700

    Task 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,11 +2,11 @@
 
 /**
  * find_listint_loop - Finds the loop contained in
- *  a listint_t linked list.
- *  @head: A pointer to the head of the listint_t list.
+ * a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
  *
- *  Return: If there is no loop - NULL.
- *  Otherwise - the address of the node where the loop starts.
+ * Return: If there is no loop - NULL.
+ * Otherwise - the address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
@@ -20,15 +20,14 @@ listint_t *find_listint_loop(listint_t *head)
 
 	while (hare)
 	{
-
 		if (tortoise == hare)
 		{
 			tortoise = head;
 
 			while (tortoise != hare)
 			{
-				while (tortoise != hare)
-					hare = hare->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
 
 			return (tortoise);
